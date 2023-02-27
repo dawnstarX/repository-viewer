48b18692dcd1e0e63ed189067f891c65ba897321
Abhinav <dawnstar15072000@gmail.com>
2023-02-28 03:59:23 +0530

    added styling to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 // @ts-nocheck
 import Head from 'next/head'
 import RedirectUser from '@/components/RedirectUser';
-import { useSession } from 'next-auth/react';
+import { useSession,signIn } from 'next-auth/react';
+import Image from 'next/image';
+import imgpath from "../assests/astronaut.svg"
 
 
 
@@ -18,16 +20,25 @@ export default  function Home () {
   }
   return (
     <>
-      <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary" onClick={() => signIn()}>Get Started</button>
-    </div>
+    <div className="flex flex-col lg:flex-row justify-center items-center h-screen">
+  <div className="flex flex-col justify-center items-center py-4 px-4 flex-shrink-0 w-full lg:w-1/3 space-y-6 mr-4 lg:mr-0">
+    <h1 className="text-4xl font-bold text-center text-white">Welcome to Repositoriz</h1>
+    <p className="text-lg text-center text-white">The new way to create,delete,update and view your repositories.</p>
+          <button className="bg-purple-700 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded" style={{ backgroundColor: '#695cce' }}
+            onClick={() => signIn()}>Get Started</button>
+
+  </div>
+
+  <div className="flex justify-center items-center flex-shrink-0 w-full lg:w-auto">
+    <Image src={imgpath} width={500} height={500} alt="Your SVG Image" />
   </div>
 </div>
+
+
+
+
+
+
 
     </>
   );
