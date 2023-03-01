2177e0ae951a920ab6d45c76d82e54616c5583d0
Abhinav <dawnstar15072000@gmail.com>
2023-03-01 19:30:16 +0530

    complete all stylings

--- components/FilterRepo.tsx
+++ components/FilterRepo.tsx
@@ -33,34 +33,42 @@ const FilterRepo = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={(event) => { handleSubmit(event) }}>
-      <label>
-        Language:
-        <select name="language" ref={languageRef}>
-          <option value="any">Any</option>
-          <option value="javascript">JavaScript</option>
-          <option value="python">Python</option>
-          <option value="java">Java</option>
-        </select>
-      </label>
-      <label>
-        Sort by:
-        <select name="sortField" ref={sortFieldRef}>
-          <option value="none">None</option>
-          <option value="name">Name</option>
-          <option value="created_at">Date created</option>
-          <option value="updated_at">Date updated</option>
-        </select>
-        <select name="sortOrder" ref={sortOrderRef}>
-          <option value="none">None</option>
-          <option value="asc">Ascending</option>
-          <option value="desc">Descending</option>
-        </select>
-      </label>
-      <button type="submit">Search</button>
+    <div className="h-60 flex flex-col justify-center items-center">
+    <h1 className="text-4xl text-white mt-2 mb-4">Filters:</h1>
+    <form className="flex flex-col sm:flex-row justify-between text-white items-center" onSubmit={(event) => { handleSubmit(event) }}>
+      <div className="flex flex-col sm:flex-row mb-2 sm:mb-0 w-full sm:w-auto">
+        <label className="flex items-center mb-2 ml-2">
+          Language:
+          <select name="language" ref={languageRef} className="ml-4 mr-4 bg-transparent border border-white rounded-md p-1 mx-2" >
+            <option value="any">Any</option>
+            <option value="javascript">JavaScript</option>
+            <option value="python">Python</option>
+            <option value="java">Java</option>
+          </select>
+        </label>
+        <label className="flex text-white items-center mb-2 ml-2">
+          Sort by:
+          <select name="sortField" ref={sortFieldRef} className="ml-4 mr-4 bg-transparent border border-white rounded-md p-1 mx-2">
+            <option value="none">None</option>
+            <option value="name">Name</option>
+            <option value="created_at">Date created</option>
+            <option value="updated_at">Date updated</option>
+          </select>
+        </label>
+        <label className="flex text-white bg-transparent items-center mb-2 ml-2">
+          Order: 
+          <select name="sortOrder" ref={sortOrderRef} className="ml-4 mr-4 bg-transparent border border-white rounded-md p-1 mx-2">
+            <option value="none">None</option>
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </label>
+      </div>
+      <button type="submit" className="py-2 px-5 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75  mb-5 sm:mt-0 mt-5 w-full sm:w-auto">Apply Filters</button>
     </form>
-    </div>
+  </div>
+  
+  
   )
 }
 
